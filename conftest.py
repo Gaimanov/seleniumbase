d4193e3875ade31bbabb85acd642ac65f46e943c
Gaimanov <80849747+Gaimanov@users.noreply.github.com>
2021-11-13 10:33:08 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -3,14 +3,14 @@ from selenium import webdriver
 from pages.login_page import LoginPage
 from pages.base_page import BasePage
 
-# options = webdriver.ChromeOptions()
-# options.add_argument('headless')
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
 
 
 @pytest.fixture(scope="function")
 def browser():
     print("\nstart browser for test..")
-    test_browser = webdriver.Chrome() #chrome_options=options)
+    test_browser = webdriver.Chrome(chrome_options=options)
     yield test_browser
     print("\nquit browser..")
     test_browser.quit()
